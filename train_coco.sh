df67e98d28c6d4e08bac0abc90015760f5ce3d38
Ahsaas Bajaj <bajajahsaas@gmail.com>
2019-11-20 21:06:02 -0500

    shell for gypsum

---
+++ train_coco.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+#SBATCH --job-name=GCN_coco
+#SBATCH --output=logs/hh_%j.txt  # output file
+#SBATCH -e logs/hh_%j.err        # File to which STDERR will be written
+#SBATCH --partition=titanx-long # Partition to submit to
+#SBATCH --gres=gpu:1
+#
+#SBATCH --ntasks=1
+
+python -u  demo_coco_gcn.py data/coco --image-size 448 --batch-size 32 -e --resume checkpoint/coco/coco_checkpoint.pth.tar
+#sleep 1
+exit
+
---
+++ train_voc.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+#SBATCH --job-name=GCN_voc
+#SBATCH --output=logs/hh_%j.txt  # output file
+#SBATCH -e logs/hh_%j.err        # File to which STDERR will be written
+#SBATCH --partition=titanx-long # Partition to submit to
+#SBATCH --gres=gpu:1
+#
+#SBATCH --ntasks=1
+
+python -u demo_voc2007_gcn.py data/voc --image-size 448 --batch-size 32 -e --resume checkpoint/voc/voc_checkpoint.pth.tar
+#sleep 1
+exit
+
