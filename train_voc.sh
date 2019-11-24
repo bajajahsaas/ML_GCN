7a11b720515891223af535a00a74765b9a237cd7
Ahsaas Bajaj <bajajahsaas@gmail.com>
2019-11-24 18:32:03 -0500

    change gpu in gypsum

--- train_voc.sh
+++ train_voc.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=GCN_voc
 #SBATCH --output=logsvoc/hh_%j.txt  # output file
 #SBATCH -e logsvoc/hh_%j.err        # File to which STDERR will be written
-#SBATCH --partition=titanx-long # Partition to submit to
+#SBATCH --partition=m40-long # Partition to submit to
 #SBATCH --gres=gpu:1
 #SBATCH --mem=40G
 #
